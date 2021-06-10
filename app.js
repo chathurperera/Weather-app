965eeebb88a4f87e4f57e8805a82190f07333828
chathura <chathuraperera007@gmail.com>
2021-06-11 05:07:27 +0530

    second commit

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const temp = document.querySelector('.temp');
 form.addEventListener('submit',async (e) => {
     e.preventDefault();
     const inputValue =  form.elements.searchTerm.value; 
-    const res =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=64853be24f58e710fa228647720351ce`)
+    const res =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=64853be24f58e710fa228647720351ce&units=metric`)
     const city = res.data.name;
+    name.innerText = city;
     const description = res.data.weather[0].description;
-    const temp = res.data.main.temp;
-    console.log(city , description , temp);
+    desc.innerText = description;
+    const temperature = res.data.main.temp;
+    temp.innerHTML = `${temperature}°C`;
+    console.log(res.data);
 })
 
