d1c038610993bf0de88b305415922dec6db923da
chathura <chathuraperera007@gmail.com>
2021-06-10 23:43:13 +0530

    updated JS

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const form = document.querySelector('form');
 const button = document.querySelector('button');
+const searchTerm = document.querySelector('#searchTerm');
+const form = document.querySelector('#input');
 
-const Userinput = document.querySelector('#userInput').value;
+const name = document.querySelector('.name');
+const desc = document.querySelector('.desc');
+const temp = document.querySelector('.temp');
 
-
-
-
-form.addEventListener('submit', async(e) => {
+form.addEventListener('submit',async (e) => {
     e.preventDefault();
-    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${Userinput}&appid=64853be24f58e710fa228647720351ce`);
-    console.log(res)
+    const inputValue =  form.elements.searchTerm.value; 
+    const res =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=64853be24f58e710fa228647720351ce`)
+    const city = res.data.name;
+    const description = res.data.weather[0].description;
+    const temp = res.data.main.temp;
+    console.log(city , description , temp);
 })
+
